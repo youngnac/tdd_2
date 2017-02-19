bd2833ee6a5749ae05102a42d39af85ac553ddaf
youngnac <youngnac@gmail.com>
2017-02-19 20:11:44 +0900

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,27 +2,55 @@ from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
-
+from lists.models import Item
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+
+        import re
+        self.assertEqual(re.sub(r"<input type='hidden' name='csrfmiddlewaretoken' value='(\w+)' />",'',response.content.decode()), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
 
-        # self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
             {'new_item_text': 'A new list item'}
         )
-        self.assertEqual(response.content.decode(), expected_html)
+        import re
+        self.assertEqual(re.sub(r"<input type='hidden' name='csrfmiddlewaretoken' value='(\w+)' />",'',response.content.decode()), expected_html)
         # self.assertTrue(response.content.endswith(b'</html>'))
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
