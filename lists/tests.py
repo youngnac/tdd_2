270fcabbce56753635b76892bf8948fe97a525e3
youngnac <youngnac@gmail.com>
2017-02-19 23:58:35 +0900

    New List model joined with Item with FK added and tests and views modified according to new model

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -22,22 +22,6 @@ class HomePageTest(TestCase):
             re.sub(r"<input type='hidden' name='csrfmiddlewaretoken' value='(\w+)' />", '', response.content.decode()),
             expected_html)
 
-        # def test_home_page_displays_all_list_items(self):
-        #     Item.objects.create(text='itemey 1')
-        #     Item.objects.create(text='itemey 2')
-        #
-        #     request = HttpRequest()
-        #     response = home_page(request)
-        #
-        #     self.assertIn('itemey 1', response.content.decode())
-        #     self.assertIn('itemey 2', response.content.decode())
-
-
-        # def test_home_page_only_saves_items_when_necessary(self):
-        #     request = HttpRequest()
-        #     home_page(request)
-        #     self.assertEqual(Item.objects.count(), 0)
-
 
 class ListViewTest(TestCase):
     def test_uses_list_templates(self):
@@ -45,8 +29,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -54,15 +39,24 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
 
-class ItemModelTest(TestCase):
+# class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -70,7 +64,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class NewListTest(TestCase):
@@ -90,4 +86,4 @@ class NewListTest(TestCase):
         )
         # self.assertRedirects(response, 'lists/the-only-list-in-the-world/')
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
