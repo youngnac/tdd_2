20a9f0f959e67cdd3448ffd07c2832df3c8d34b8
youngnac <youngnac@gmail.com>
2017-02-19 16:15:09 +0900

    second item test added (three stirkes...)

--- functional_tests.py
+++ functional_tests.py
@@ -20,21 +20,32 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
 
-        inputbox.send_keys('Buy peacock feathers')
+        # inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Use peacock feathers to make a fly')
 
         inputbox.send_keys(Keys.ENTER)
 
+        # import time
+        # time.sleep(10)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table -- its text was:\n%s" % (
+        #         table.text,
+        #     )
+        # )
 
         self.fail('Finish the test!')
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,28 @@
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-
-
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+
+        # self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+        # self.assertTrue(response.content.endswith(b'</html>'))
